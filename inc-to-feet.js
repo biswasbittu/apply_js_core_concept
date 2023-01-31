8749104fd23efc4ba6b9aead0f7b9fe7874217e9
biswasbittu <biswasbittu7415@yahoo.com>
2023-01-31 15:30:58 +0530

    apply function

---
+++ inc-to-feet.js
@@ -0,0 +1,19 @@
+// const userHeight=144;
+
+// function incToFeet(inches){
+//     const feet=inches/12;
+//     return feet;
+// }
+
+// const userHeightInFeet=incToFeet(userHeight);
+// console.log(userHeightInFeet);
+
+function mitterToCm(cm){
+    const mitter=cm*100;
+    return mitter;
+
+}
+
+const givenCm=2;
+const finalresult=mitterToCm(givenCm);
+console.log(finalresult,'c.m');
